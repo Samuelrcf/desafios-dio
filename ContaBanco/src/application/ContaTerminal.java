e5401cd2049784af9ae6b3ee45caba601d170d66
Samuel Rógenes <rogenescarvalho@hotmail.com>
2024-08-03 14:44:46 -0300

    launching projects

---
+++ ContaBanco/src/application/ContaTerminal.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); 
+        
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldo = scanner.nextDouble();
+        
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                          agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        
+        System.out.println(mensagem);
+        
+        scanner.close();
+    }
+}
---
+++ Contador/src/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
